29e78015b9ec1b2a020ab7ff445d136883d5845d
LeonardoCastro09 <113216006+LeonardoCastro09@users.noreply.github.com>
2022-11-02 17:35:51 -0500

    Add files via upload

---
+++ Programa.py
@@ -0,0 +1,37 @@
+class Persona:
+    def __init__(self):
+        self.nombre=""
+        self.edad=int
+        self.genero=""
+        self.volumen=""
+        self.bigote=""
+        
+    def gritar(self):
+        pass
+        
+    def Bailar(self):
+        pass
+        
+    def Saltar(self):
+        pass
+    
+sujetoA=Persona()
+sujetoA.nombre="Camilo"
+sujetoA.edad=20
+sujetoA.genero="Pansexual"
+sujetoA.volumen="Alto"
+sujetoA.bigote=False
+    
+sujetoB=Persona()
+sujetoA.nombre="Cristian"
+sujetoA.edad=56
+sujetoA.genero="Masculino"
+sujetoA.volumen="Medio"
+sujetoA.bigote=False
+
+sujetoC=Persona()
+sujetoA.nombre="Lina"
+sujetoA.edad=19
+sujetoA.genero="Femenino"
+sujetoA.volumen="Medio"
+sujetoA.bigote=False
